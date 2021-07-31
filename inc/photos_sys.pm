8ce1944ba44d18a2dfd9991f2320b0f0ebcbdc7b
Eric Grivel <github-com@lumenssolutions.com>
2021-07-31 17:47:50 -0400

    2.4.3: when starting slide show, start with next image

--- inc/photos_sys.pm
+++ inc/photos_sys.pm
@@ -15,7 +15,7 @@ my @sys_fields = ("syskey", "sysvalue");
 # Version 2.2.0: allow start and end for slide show to be set
 # Version 2.3.0: allow going back in slideshow
 # Version 2.4.0: start/stop and settings in slideshow
-my $VERSION = "2.4.2";
+my $VERSION = "2.4.3";
 
 sub psys_init {
   psql_init();
