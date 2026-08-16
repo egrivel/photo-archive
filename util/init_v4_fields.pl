34d3ced8aab31198c8636cb30695ff06bef3dd9e
Eric Grivel <eric@lincoln.grivel.bogus>
2026-08-16 07:19:30 -0400

    Updated init v4 fields with more checks and output

--- util/init_v4_fields.pl
+++ util/init_v4_fields.pl
@@ -47,6 +47,21 @@ if (!$gl_done_anything) {
   print "Didn't do anything...\n";
 }
 
+sub format_timestamp {
+  my $time = $_[0];
+
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($time);
+  $year += 1900;
+  $mon++;
+  $mon = "0$mon" if ($mon < 10);
+  $mday = "0$mday" if ($mday < 10);
+  $hour = "0$hour" if ($hour < 10);
+  $min = "0$min" if ($min < 10);
+  $sec = "0$sec" if ($sec < 10);
+
+  return "$year-$mon-$mday $hour:$min:$sec";
+}
+
 sub show_help {
   print "Initialize the database v4 field:\n";
   print "origWidth, origHeight, editWidth, editHeight, addedDateTime\n";
@@ -59,13 +74,15 @@ sub do_image {
 
   if (!pcom_is_valid($imageId)) {
     # invalid image ID
-    print "Invalid image ID for $imageId.\n";
+    print "$imageId: invalid image ID.\n";
     return;
   }
 
   pdb_open_image($imageId);
   if (!pdb_image_info($imageId)) {
-    print "Image $imageId does not exist in the database.\n";
+    # I don't think this can happen after opening the image, but keeping this
+    # just in case
+    print "$imageId: does not exist in the database.\n";
     return;
   }
 
@@ -74,10 +91,21 @@ sub do_image {
   if ($orig eq "") {
     $orig = $raw;
   }
-  if ($orig eq "") {
-    print "Cannot find orig for $imageId; every image should have an original.\n";
-  }
   my $edited = pfs_get_edited_location($imageId);
+
+  if (($orig eq "") && ($edited ne "")) {
+    # It looks like some variations (..a, ..b etc. suffixes) have been saved
+    # as edited files without corresponding original. Warn on that; at some
+    # point, the archive should be cleaned up to move these edited to become
+    # the original.
+    $orig = $edited;
+    print "$imageId: using edited as original.\n" if ($gl_verbose);
+  }
+
+  if ($orig eq "") {
+    print "$imageId: cannot find orig; every image should have an original.\n";
+    return;
+  }
 
   ($origWidth, $origHeight) = pfs_get_file_dimensions($orig);
   print "Got file dimensions $origWidth x $origHeight for $orig\n" if ($gl_verbose);
@@ -85,23 +113,97 @@ sub do_image {
     print "$imageId: orig dimensions are $origWidth x $origHeight\n";
   }
 
+  my $rotation = pdb_get_rotation($imageId);
+  # Orientation values:
+  # $PCOM_LANDSCAPE = "landscape";
+  # $PCOM_PORTRAIT = "portrait";
+  # $PCOM_FREEFORM = "freeform";
+  # $PCOM_FREEFORM_P = "freeform-p";
+  # $PCOM_FREEFORM_L = "freeform-l";
+  my $orientation = pdb_get_orientation($imageId);
+
   my $editedWidth;
   my $editedHeight;
   if ($edited eq "") {
-    # Use orig width, but apply rotation if needed
-    my $rotation = pdb_get_rotation($imageId);
-    if (($rotation eq "90") || ($rotation eq "270")) {
-      $editedWidth = $origHeight;
-      $editedHeight = $origWidth;
+    # No edited file to get dimensions from, so default to copying the orig
+    # values
+    if ($origWidth && $origHeight) {
+      # Try using the original's dimensions to determine what the edited
+      # dimensions would be.
+
+      if ($origHeight > $origWidth) {
+        # Original is already portrait, so use directly. Rotation should be
+        # ignored for portrait originals. However, check with the recorded
+        # orientation
+        if (($orientation eq $PCOM_PORTRAIT)
+          # Freeform can be either landscape or portrait, so is valid for a
+          # portrait image
+          || ($orientation eq $PCOM_FREEFORM)
+          || ($orientation eq $PCOM_FREEFORM_P)) {
+          $editedWidth = $origWidth;
+          $editedHeight = $origHeight;
+        } else {
+          print "$imageId: portrait original, no edited, recorded as landscape.\n";
+        }
+      } else {
+        # Original is landscape, potentially the rotation applies. Only rotation
+        # values of 90 or 270 indicate changing of orientation, all others leave
+        # the orientation in place
+        if (($rotation eq "90") || ($rotation eq "270")) {
+          # The rotation says to rotate the original before rendering. Checking
+          # to see that the orientation matches
+          if (($orientation eq $PCOM_PORTRAIT)
+            # Freeform can be either landscape or portrait, so is valid for a
+            # portrait image
+            || ($orientation eq $PCOM_FREEFORM)
+            || ($orientation eq $PCOM_FREEFORM_P)) {
+            $editedWidth = $origHeight;
+            $editedHeight = $origWidth;
+          } else {
+            print "$imageId: has rotation set but orientation is landscape.\n";
+          }
+        } else {
+          # No rotation, so should stay landscape. Checking to see that the
+          # orientation matches
+          if (($orientation eq $PCOM_LANDSCAPE)
+            # Freeform can be either landscape or portrait, so is valid for a
+            # landscape image
+            || ($orientation eq $PCOM_FREEFORM)
+            || ($orientation eq $PCOM_FREEFORM_L)) {
+            $editedWidth = $origWidth;
+            $editedHeight = $origHeight;
+          } else {
+            print "$imageId: has rotation set but orientation is landscape.\n";
+          }
+        }
+      }
     } else {
-      $editedWidth = $origWidth;
-      $editedHeight = $origHeight;
+      print "$imageId: no edited file, and no valid orig dimensions.\n";
     }
   } else {
     ($editedWidth, $editedHeight) = pfs_get_file_dimensions($edited);
-  }
-  if (!$editedWidth || !$editedHeight) {
-    print "$imageId: edited dimensions are $editedWidth x $editedHeight\n";
+    if ($editedWidth && $editedHeight) {
+      # Check if the orientation matches
+      if ($editedHeight > $editedWidth) {
+        if (($orientation ne $PCOM_PORTRAIT)
+          # Freeform can be either landscape or portrait, so is valid for a
+          # portrait image
+          && ($orientation ne $PCOM_FREEFORM)
+          && ($orientation ne $PCOM_FREEFORM_P)) {
+          print "$imageId: edited is portrait but recorded as landscape.\n";
+        }
+      } elsif ($editedWidth > $editedHeight) {
+        if (($orientation ne $PCOM_LANDSCAPE)
+          # Freeform can be either landscape or portrait, so is valid for a
+          # landscape image
+          && ($orientation ne $PCOM_FREEFORM)
+          && ($orientation ne $PCOM_FREEFORM_L)) {
+          print "$imageId: edited is landscape but recorded as portrait.\n";
+        }
+      }
+    } else {
+      print "$imageId: edited dimensions are $editedWidth x $editedHeight\n";
+    }
   }
   print "Got file dimensions $editedWidth x $editedHeight for $edited\n" if ($gl_verbose);
 
@@ -123,16 +225,8 @@ sub do_image {
   if (!$time) {
     print "$imageId: no time found\n";
   }
-  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime($time);
-  $year += 1900;
-  $mon++;
-  $mon = "0$mon" if ($mon < 10);
-  $mday = "0$mday" if ($mday < 10);
-  $hour = "0$hour" if ($hour < 10);
-  $min = "0$min" if ($min < 10);
-  $sec = "0$sec" if ($sec < 10);
 
-  my $addedDateTime = "$year-$mon-$mday $hour:$min:$sec";
+  my $addedDateTime = format_timestamp($time);
   print "$addedDateTime for this file\n" if ($gl_verbose);
 
   pdb_set_orig_width($origWidth);
@@ -168,6 +262,10 @@ sub do_year {
 }
 
 sub do_all {
+  my $start = time();
+  print "Starting " . format_timestamp($start) . "\n";
+  print "-------------------------------------------------------------------\n";
+
   # The iter must be created with a potentially valid image ID. Since film
   # sets come first, "001" is the first possible set ID and "00100" is the
   # first possible image ID
@@ -181,6 +279,19 @@ sub do_all {
     last if ($imageId eq "");
   }
   pdb_iter_done($img_iter);
+
+  my $end = time();
+  print "-------------------------------------------------------------------\n";
+  print "Ending " . format_timestamp($end) . "\n";
+  my $seconds = $end - $start;
+  my $minutes = int($seconds / 60);
+  $seconds -= 60 * $minutes;
+  my $hours = int($minutes / 60);
+  $minutes -= 60 * $hours;
+  my $days = int($hours / 24);
+  $hours -= 24 * $days;
+  print "Time to run: $days days, $hours hours, $minutes minutes, $seconds seconds\n";
+  print "-------------------------------------------------------------------\n";
 }
 
 sub check_image {
