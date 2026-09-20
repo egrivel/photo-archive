e5e83945f44838f29b738b076df563ad159bdf5d
Eric Grivel <github-com@lumenssolutions.com>
2026-09-20 17:28:00 -0400

    Allow use of linked scripts

--- cgi-bin/inc_all.pm
+++ cgi-bin/inc_all.pm
@@ -1,4 +1,9 @@
-my $script = __FILE__;
+use Cwd;
+use Cwd 'abs_path';
+
+# By using the abs_path, it is possible to create a directory with just a few
+# linked scripts to access the functionality of the full photo archive.
+my $script = abs_path(__FILE__);
 my $localdir = ".";
 if ($script =~ s/\/[^\/]*$//) {
   $localdir = $script;
